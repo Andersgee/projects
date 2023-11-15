54658cb1d0941b85b6dae6ba89c20cc8a28fbb39
andy <andersgee@gmail.com>
2023-11-15 19:29:09 +0100

    priority on second card image aswell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,7 @@ export default function Page() {
             />
 
             <Card
+              priority
               href="https://musker.andyfx.net"
               imageUrl="/projectimages/musker.webp"
               title="Musker"
